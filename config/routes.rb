f42108e7e5979b1b6ddf862d498a350798225f5d
Kurtis Clauser <kclauser@gmail.com>
2015-10-29 17:08:16 -0400

    groups controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'topics#index'
+  root '#index'
 
   get    '/auth/:provider'          => 'omniauth#auth',    as: :auth
   get    '/auth/:provider/callback' => 'sessions#create'
