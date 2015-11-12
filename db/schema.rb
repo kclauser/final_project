e24a1962c3f2ba6740ba8ece5ea1c1600d07c427
Kurtis Clauser <kclauser@gmail.com>
2015-11-11 21:32:37 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109230149) do
+ActiveRecord::Schema.define(version: 20151112022047) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +30,12 @@ ActiveRecord::Schema.define(version: 20151109230149) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "refile_attachments", force: :cascade do |t|
+    t.string "namespace", null: false
+  end
+
+  add_index "refile_attachments", ["namespace"], name: "index_refile_attachments_on_namespace", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
